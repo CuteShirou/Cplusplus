2b9b74376c07d862a70f94b1ab90176a5cb50f34
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-04-22 12:48:15 +0200

    Update Rectangle.cpp

--- C++/Source/Rectangle.cpp
+++ C++/Source/Rectangle.cpp
@@ -2,10 +2,36 @@
 
 Rectangle::Rectangle()
 {
+	rectWidth = 0;
+	rectHeight = 0;
+	rectx = 0;
+	recty = 0;
 }
 
-Rectangle::Rectangle(int width, int height, int x, int y)
+Rectangle::Rectangle(int width, int height, int x, int y) : rectWidth(width), rectHeight(height), rectx(x), recty(y)
 {
+}
+
+void Rectangle::PoseSet(int x, int y)
+{
+	rectx = x;
+	recty = y;
+}
+
+void Rectangle::SizeSet(int width, int height)
+{
+	rectWidth = width;
+	rectHeight = height;
+}
+
+int Rectangle::GetPose()
+{
+	return rectx, recty;
+}
+
+int Rectangle::GetSize()
+{
+	return rectWidth, rectHeight;
 }
 
 Rectangle::~Rectangle()
