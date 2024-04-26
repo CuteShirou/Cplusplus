a3c5a7c5cad5c8e0a98cbd924972269efeaaff88
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-04-26 11:24:50 +0200

    Update Obstacle.h

--- C++ Console/Header/Obstacle.h
+++ C++ Console/Header/Obstacle.h
@@ -1,8 +1,37 @@
+#pragma once
+
 class Obstacle
 {
 public:
-	Obstacle();
-	virtual ~Obstacle();
-protected:
-	bool IsCollisionObstacle();
+    Obstacle();
+    virtual ~Obstacle();
+
+    virtual bool IsCollision(int PlayerX, int PlayerY) = 0;
+};
+
+class Pothole : public Obstacle
+{
+public:
+    Pothole();
+    virtual ~Pothole();
+
+    virtual bool IsCollision(int PlayerX, int PlayerY);
+};
+
+class Car : public Obstacle
+{
+public:
+    Car();
+    virtual ~Car();
+
+    virtual bool IsCollision(int PlayerX, int PlayerY);
+};
+
+class Person : public Obstacle
+{
+public:
+    Person();
+    virtual ~Person();
+
+    virtual bool IsCollision(int PlayerX, int PlayerY);
 };
