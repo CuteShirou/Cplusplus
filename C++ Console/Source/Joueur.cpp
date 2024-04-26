2949c3be30fd4af6a64ef4488ed9cefbedf66055
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-04-26 11:24:47 +0200

    Update Joueur.cpp

--- C++ Console/Source/Joueur.cpp
+++ C++ Console/Source/Joueur.cpp
@@ -1,9 +1,43 @@
 #include "Joueur.h"
 
-Joueur::Joueur()
+Joueur::Joueur(int screenWidth) : screenWidth(screenWidth)
 {
+    x = screenWidth / 2;
 }
 
 Joueur::~Joueur()
 {
+}
+
+void Joueur::MoveRight()
+{
+    x++;
+    if (x >= screenWidth)
+    {
+        x = screenWidth - 1;
+    }
+}
+
+void Joueur::MoveLeft()
+{
+    x--;
+    if (x < 0)
+    {
+        x = 0;
+    }
+}
+
+bool Joueur::IsCollision(int obstacleX, int obstacleY)
+{
+    return (x == obstacleX && 0 == obstacleY);
+}
+
+int Joueur::PlayerX() const
+{
+    return x;
+}
+
+int Joueur::PlayerY() const
+{
+    return 0;
 }
