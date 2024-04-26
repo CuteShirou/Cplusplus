133f8da51ba40b4cce63cf936cb9b84107717337
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-04-26 11:24:43 +0200

    Update Obstacle.cpp

--- C++ Console/Source/Obstacle.cpp
+++ C++ Console/Source/Obstacle.cpp
@@ -1,42 +1,26 @@
 #include "Obstacle.h"
 
-Obstacle::Obstacle()
-{
-}
-Obstacle::~Obstacle()
-{
-}
+Obstacle::Obstacle() {}
+Obstacle::~Obstacle() {}
 
-Pothole::Pothole()
-{
-}
-Pothole::~Pothole()
-{
-}
+Pothole::Pothole() {}
+Pothole::~Pothole() {}
 
 bool Pothole::IsCollision(int playerX, int playerY)
 {
     return false;
 }
 
-Car::Car()
-{
-}
-Car::~Car()
-{
-}
+Car::Car() {}
+Car::~Car() {}
 
 bool Car::IsCollision(int playerX, int playerY)
 {
     return false;
 }
 
-Person::Person()
-{
-}
-Person::~Person()
-{
-}
+Person::Person() {}
+Person::~Person() {}
 
 bool Person::IsCollision(int playerX, int playerY)
 {
