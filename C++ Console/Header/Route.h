a6a9c38740daf3af0bea7fe8d3031447b59635f7
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-04-26 10:27:55 +0200

    Create Route.h

---
+++ C++ Console/Header/Route.h
@@ -0,0 +1,28 @@
+#include <vector>
+
+class  Route
+{
+public:
+	Route();
+
+	void SetRouteX(int X);
+	int GetRouteX();
+
+	void SetRouteY(int Y);
+	int GetRouteY();
+
+	void SetRouteGlobale(int index, int value);
+	int GetRouteGlobale(int index);
+
+	void MoveRight();
+	void MoveLeft()
+
+	bool IsPlayerOnRoad(int playerX, int PlayerY);
+
+	virtual ~Route();
+protected:
+	bool IsOnRoad();
+
+private:
+	std::vector<int> routeGlobale;
+};
